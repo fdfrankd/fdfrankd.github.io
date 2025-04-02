904b0b9c22a1fd246b6e3f9699dfa152cab46834
fdfrankd <fdqueralta@my.waketech.edu>
2025-04-02 17:01:39 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,14 @@
+// gradebook.js
+
+console.log("Gradebook script loaded");
+
+function calculateFinalGrade(grades) {
+    console.log("calculateFinalGrade called with:", grades);
+    // Placeholder for calculation logic
+    return 0;
+}
+
+function addStudentRow(name, grades) {
+    console.log("addStudentRow called with:", name, grades);
+    // Placeholder for row-adding logic
+}
